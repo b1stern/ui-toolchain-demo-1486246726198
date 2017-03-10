3661204bdbed23c094d641eadb06546334ac6d29
b1stern <bstern@us.ibm.com>
2017-03-10 07:58:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -100,7 +100,8 @@ function orderItem(itemID){
 	</div>
 	<div id="loading"><br>Loading...</div>
 	<a href="./autoLoadTest.html">Catalog Load Tester</a>
-	 <img src="http://upload.wikimedia.org/wikipedia/commons/2/26/Invalidenpark%2C_Berlin-Mitte%2C_360x180%2C_160326%2C_ako.jpg" height="100%" width="100%" alt="Oops">
+	 <!--img src="http://upload.wikimedia.org/wikipedia/commons/2/26/Invalidenpark%2C_Berlin-Mitte%2C_360x180%2C_160326%2C_ako.jpg" height="100%" width="100%" alt="Oops"-->
+	 <img src="https://en.wikipedia.org/wiki/Small_Faces#/media/File:Small_Faces_1965.JPG" height=150 width=120>
 </body>
 </html>
 
